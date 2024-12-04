e6a919447a41201eb17864fdd6ddbc0b78e8d22f
guoguozhenhaowan <877801999@qq.com>
2024-12-04 07:44:03 +0000

    固定python版本为python3.9，固定prometheus-client版本为0.16.0
    Signed-off-by: guoguozhenhaowan <877801999@qq.com>

--- package.sh
+++ package.sh
@@ -11,7 +11,7 @@ else
 	echo 'Not supported platform, please download the compiled nodejs and Python runtime manually.'
 	exit 1
 fi
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-${platform}.sh -O miniconda.sh --no-check-certificate && sh miniconda.sh -b -p python
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_24.4.0-0-Linux-${platform}.sh -O miniconda.sh --no-check-certificate && sh miniconda.sh -b -p python
 wget https://nodejs.org/dist/v16.9.0/node-v16.9.0-linux-${version}.tar.xz --no-check-certificate && tar -xJvf node-v16.9.0-linux-${version}.tar.xz 
 export CI=False
 export PATH=`pwd`/node-v16.9.0-linux-${version}/bin:`pwd`/python/bin:$PATH
